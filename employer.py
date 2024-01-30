154bb190d9e2c8da3a9c1b418af8785556632dcb
tumbaymba <54357075+tumbaymba@users.noreply.github.com>
2024-01-30 20:46:13 +0300

    Update employer.py

--- employer.py
+++ employer.py
@@ -1,16 +1,1 @@
-import requests
-import os
-import json
 
-class Employer:
-
-    def employee_of_date(self):
-        values = {'text': 'газ', 'area': 1, 'only_with_vacancies': True}
-
-        req = requests.get('https://api.hh.ru/employers', values)
-        data = req.json()
-
-        employees_file = 'employers.json'
-        with open(employees_file, 'w') as f:
-            json.dump(data, f, ensure_ascii=False)
-        return data
